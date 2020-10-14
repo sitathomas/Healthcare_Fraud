8ad5f512d9e6bfc28b5a0a79d964cd80bd363f28
Sita Thomas <sita.thomas@gmail.com>
2020-10-14 19:28:29 -0400

    Sita - update Pre-processing

--- Preprocessing.py
+++ Preprocessing.py
@@ -2,7 +2,7 @@ import sys
 sys.path.insert(0, '.')
 
 import pandas as pd
-import Sita_Functions as fxns
+import Functions as fxns
 import numpy as np
 from joblib import dump
 
@@ -13,7 +13,7 @@ outpatient =  pd.read_csv('./data/Train_Outpatientdata-1542865627584.csv')
 target = pd.read_csv('./data/Train-1542865627584.csv')
 
 # change numeric encoding from 1/2 to 0/1
-fxns.re_encode(beneficiary, inpatient, outpatient)
+fxns.re_encode(beneficiary)
 
 # numerically encode RenalDiseaseIndicator
 beneficiary.loc[beneficiary.RenalDiseaseIndicator == '0', 'RenalDiseaseIndicator'] = 0
